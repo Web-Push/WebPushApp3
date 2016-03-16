6110b72911b66965e4d122c2ce31962fd8347d40
Web-Push <cloudlearning777@gmail.com>
2016-03-16 13:47:20 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,30 @@
 'use strict';
 
-self.importScripts('https://web-push.github.io/k_y_test/service-worker.js');
+console.log('WebPushApp3 servicewoker load');
+
+
+self.importScripts('https://web-push.github.io/postmessageTest/service-worker.js');
+
+
+self.addEventListener('notificationclick', function(event) {
+  console.log('WebPushApp3 On notification click: ', event.notification.tag);
+  // Android doesn’t close the notification when you click on it
+  // See: http://crbug.com/463146
+  event.notification.close();
+
+  // This looks to see if the current is already open and
+  // focuses if it is
+  event.waitUntil(clients.matchAll({
+    type: 'window'
+  }).then(function(clientList) {
+    for (var i = 0; i < clientList.length; i++) {
+      var client = clientList[i];
+      if (client.url === '/' && 'focus' in client) {
+        return client.focus();
+      }
+    }
+    if (clients.openWindow) {
+      return clients.openWindow('/WebPushApp3');
+    }
+  }));
+});
